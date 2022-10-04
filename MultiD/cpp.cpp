47ae6091d006ebf198dcb413a1e4556917793ec5
HP1717\91700 <raja_2020bece042@nitsri.net>
2022-10-04 23:47:09 +0530

    -

--- MultiD/cpp.cpp
+++ MultiD/cpp.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 int main()
 {
-    cout<<"Hello, Georgie";
+    cout<<"Hello, Georgie"<<endl;
+
+    int oneD[12];
+
+    int idx=0;
+
+        for(int i=0;i<12;i++)
+        {
+            cout<< "oneD "<<i<<" : "<<"2D : "<<i/4<<" : "<<i%4<<" \t";
+        }
 }
